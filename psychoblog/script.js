95e2c8a5001adad119e23f285b95e2f0c99a1dfa
Gaulix <36313076+Gaulix@users.noreply.github.com>
2024-08-09 18:49:30 +0200

    Delete psychoblog directory

--- psychoblog/script.js
+++
@@ -1,6 +0,0 @@
-let burger = document.getElementById('burger');
-const BODY = document.querySelector('body');
-
-burger.addEventListener('click', function() {
-    BODY.classList.toggle('active');
-});
